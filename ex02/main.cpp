9a1fe631583f2ce2c639b9a256d0c833a9eb95e0
roblabla <robinlambertz+dev@gmail.com>
2015-06-18 21:11:45 +0000

    Fix main of ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: roblabla </var/spool/mail/roblabla>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/06/12 20:43:33 by roblabla          #+#    #+#             */
-/*   Updated: 2015/06/14 21:11:04 by roblabla         ###   ########.fr       */
+/*   Updated: 2015/06/18 21:11:24 by roblabla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,16 @@
 
 int main( void ) {
 	Fixed a;
-	a.setRawBits(0x307);
-
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
 	std::cout << a << std::endl;
-	std::cout << (Fixed(12.6f) * Fixed(4.2f)) << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
 	std::cout << a++ << std::endl;
 	std::cout << a << std::endl;
+
 	std::cout << b << std::endl;
+
 	std::cout << Fixed::max( a, b ) << std::endl;
 
 	return 0;
